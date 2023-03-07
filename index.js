fc8d5221f7cdbeff5e63a5241d0d3d0310c7ee69
naveenao <naveenthekingmaker1@gmail.com>
2023-03-07 10:29:51 +0530

    added argo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send('Check run from end to end deployment from jenkins. Hi Naveen!!Welcome to the jungle');
+    res.send('Argo cd is monitoring after deployment');
 });
 
 app.listen(8082, () => {
